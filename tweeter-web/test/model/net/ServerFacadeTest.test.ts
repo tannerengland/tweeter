1273db303bd47f6c00f34e0a054bc5afe54b2b44
Tanner England <tannerengland1@gmail.com>
2024-12-06 16:58:58 -0700

    4b no tests

--- tweeter-web/test/model/net/ServerFacadeTest.test.ts
+++ tweeter-web/test/model/net/ServerFacadeTest.test.ts
@@ -1,71 +1,71 @@
-import "isomorphic-fetch";
-import { instance, mock, verify, spy, when } from "ts-mockito";
-import { AuthToken, GetFollowCountRequest, PagedUserItemRequest, RegisterRequest, UserDto } from "tweeter-shared";
-import { ServerFacade } from "../../../src/network/ServerFacade";
+// import "isomorphic-fetch";
+// import { instance, mock, verify, spy, when } from "ts-mockito";
+// import { AuthToken, GetFollowCountRequest, PagedUserItemRequest, RegisterRequest, UserDto } from "tweeter-shared";
+// import { ServerFacade } from "../../../src/network/ServerFacade";
 
-describe("ServerFacade", () => {
-    let currServerFacade = new ServerFacade();
+// describe("ServerFacade", () => {
+//     let currServerFacade = new ServerFacade();
 
-    it("correctly registers a user", async () => {
-        let request: RegisterRequest = {
-            firstName: "John",
-            lastName: "Doe",
-            alias: "johnDoe123",
-            password: "securePassword123",
-            userImageBytes: "aW1hZ2VCeXRlcyBhcyBhIHN0cmluZw==",
-            imageFileExtension: "jpg"
-        }
+//     it("correctly registers a user", async () => {
+//         let request: RegisterRequest = {
+//             firstName: "John",
+//             lastName: "Doe",
+//             alias: "johnDoe123",
+//             password: "securePassword123",
+//             userImageBytes: "aW1hZ2VCeXRlcyBhcyBhIHN0cmluZw==",
+//             imageFileExtension: "jpg"
+//         }
 
-        const [user, authToken] = await currServerFacade.register(request);
+//         const [user, authToken] = await currServerFacade.register(request);
 
-        expect(user).not.toBeNull();
-        expect(authToken).not.toBeNull();
-    });
+//         expect(user).not.toBeNull();
+//         expect(authToken).not.toBeNull();
+//     });
 
-    it("correctly gets followers", async () => {
-        let currUser: UserDto = {
-            alias: "userAlias123",
-            firstName: "John",
-            lastName: "Doe",
-            imageUrl: "https://example.com/userImage.jpg"
-          }
+//     it("correctly gets followers", async () => {
+//         let currUser: UserDto = {
+//             alias: "userAlias123",
+//             firstName: "John",
+//             lastName: "Doe",
+//             imageUrl: "https://example.com/userImage.jpg"
+//           }
 
-        let request: PagedUserItemRequest = {
-            token: "secret",
-            userAlias: "dude",
-            pageSize: 10,
-            lastItem: currUser
-        }
+//         let request: PagedUserItemRequest = {
+//             token: "secret",
+//             userAlias: "dude",
+//             pageSize: 10,
+//             lastItem: currUser
+//         }
 
-        const [users, hasMore] = await currServerFacade.getMoreFollowees(request);
+//         const [users, hasMore] = await currServerFacade.getMoreFollowees(request);
 
-        expect(users).not.toBeNull();
-    });
+//         expect(users).not.toBeNull();
+//     });
 
-    it("correctly gets follower/following count", async () => {
-        let currUser: UserDto = {
-            alias: "userAlias123",
-            firstName: "John",
-            lastName: "Doe",
-            imageUrl: "https://example.com/userImage.jpg"
-          }
+//     it("correctly gets follower/following count", async () => {
+//         let currUser: UserDto = {
+//             alias: "userAlias123",
+//             firstName: "John",
+//             lastName: "Doe",
+//             imageUrl: "https://example.com/userImage.jpg"
+//           }
 
 
-        let request: GetFollowCountRequest = {
-            token: "beans",
-            user: currUser
-        }
+//         let request: GetFollowCountRequest = {
+//             token: "beans",
+//             user: currUser
+//         }
 
-        const followerCount = await currServerFacade.getFollowerCount(request);
+//         const followerCount = await currServerFacade.getFollowerCount(request);
 
-        const followeeCount = await currServerFacade.getFolloweeCount(request);
+//         const followeeCount = await currServerFacade.getFolloweeCount(request);
 
-        expect(followerCount).not.toBeNull();
-        expect(typeof followerCount).toBe('number');
-        expect(followeeCount).not.toBeNull();
-        expect(typeof followeeCount).toBe('number');
+//         expect(followerCount).not.toBeNull();
+//         expect(typeof followerCount).toBe('number');
+//         expect(followeeCount).not.toBeNull();
+//         expect(typeof followeeCount).toBe('number');
 
-    });
-});
+//     });
+// });
 
-// functionality, tests, 2 end to end componenet presenter webservice serfacade clientcomm lambda serverservice
+// // functionality, tests, 2 end to end componenet presenter webservice serfacade clientcomm lambda serverservice
